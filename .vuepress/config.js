937f1d30fac22f54a4f642448ef9d9f2910a379f
wanghan <1594078165@qq.com>
2022-02-13 23:21:33 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 const sidebar = require('./siderbar.js');
 module.exports = {
-  "title": "梨子的博客",
+  "title": "猫腻的博客",
   "description": "记录我的学习日常",
   "dest": "docs",
   "base": '/my_blog/',
@@ -57,7 +57,7 @@ module.exports = {
         "items": [
           {
             "text": "GitHub",
-            "link": "https://github.com/lizi1225",
+            "link": "https://github.com/maoni1225",
             "icon": "reco-github"
           }
         ]
@@ -87,7 +87,7 @@ module.exports = {
     "search": true,
     "searchMaxSuggestions": 10,
     "lastUpdated": "Last Updated",
-    "author": "lizi",
+    "author": "maoni",
     "authorAvatar": "/avatar.jpg",
     // "record": "xxxx",
     "startYear": "2021"
