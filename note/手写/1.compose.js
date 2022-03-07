2420d30041e3d38bcf5d6c570187c7a0727cd337
wanghan <wanghan@sina.com>
2022-03-07 18:42:55 +0800

    手写

--- note/手写/1.compose.js
+++ note/手写/1.compose.js
@@ -1,15 +1,16 @@
+// https://juejin.cn/post/6968713283884974088
 function fn1(x) {
     return x + 1;
   }
-  function fn2(x) {
-    return x + 2;
-  }
-  function fn3(x) {
-    return x + 3;
-  }
-  function fn4(x) {
-    return x + 4;
-  }
+function fn2(x) {
+  return x + 2;
+}
+function fn3(x) {
+  return x + 3;
+}
+function fn4(x) {
+  return x + 4;
+}
 function compose(...fns) {
     if (!fns.length) return v => v
     if (fns.length === 1) {
