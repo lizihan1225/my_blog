717e014eb1dcb3cd0d7434055b8493ad7f595206
wanghan <1594078165@qq.com>
2024-09-24 01:05:28 +0800

    feat: chrome

---
+++ note/chrome/scripts/content.js
@@ -0,0 +1,27 @@
+const reg = /project\/(\d+)/;
+const projecId = location.href.match(reg)[1];
+
+const projecIdMap = {
+  2744361: "mojave",
+};
+
+function getInfo() {
+  const method = document.querySelector(
+    ".pui-g-ui-kit-request-method-icon-index-container"
+  );
+
+  const path = document.querySelector(
+    ".pui-pages-api-api-manage-http-http-api-case-components-path-input-text pui-pages-api-api-manage-http-http-api-case-index-path-hove"
+  );
+
+  const desc = document.querySelector(".name-row-item-value");
+  console.log(method, path, desc);
+  return method;
+}
+
+let timer = setInterval(() => {
+  if (getInfo()) {
+    clearInterval(timer);
+    timer = null;
+  }
+}, 1000);
